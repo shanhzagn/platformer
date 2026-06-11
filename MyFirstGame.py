53c0f1bce0bec9c176cacce40acbca680cd09c02
shanhzagn <shawn.s.zhang.2011@gmail.com>
2026-06-11 21:07:02 +1000

    created blank window and clock

---
+++ MyFirstGame.py
@@ -0,0 +1,22 @@
+import pygame
+from sys import exit
+
+pygame.init()
+screen = pygame.display.set_mode((800,400))
+pygame.display.set_caption("Parkour Jumpgi")
+clock = pygame.time.Clock()
+
+test_surface = pygame.Surface((100,200))
+test_surface.fill("Red")
+test_image = pygame.image.load("")
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    screen.blit(test_surface,(350,100))
+
+    pygame.display.update()
+    clock.tick(60)
