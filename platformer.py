ffec00e620532105655ecb351ce75d51722931fa
shanhzagn <shawn.s.zhang.2011@gmail.com>
2026-06-15 15:02:58 +1000

    added moving and jumping

--- platformer.py
+++ platformer.py
@@ -18,12 +18,26 @@ player_surface = pygame.Surface((30,30))
 player_surface.fill("blue")
 player_rect = player_surface.get_rect(bottomleft = (150,380))
 player_gravity = 0
+player_x = 0
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                player_gravity = -15
+        if player_rect.x >= 0:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    player_x = -15
+        if player_rect.x <= 800:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    player_x = 15
+
+
 
     screen.blit(background_surface,(0,0))
 
@@ -34,6 +48,15 @@ while True:
     if player_rect.colliderect(floor_rect):
         player_rect.bottom = floor_rect.top
         player_gravity = 0
+    if player_rect.x < 0:
+        player_x = 0
+        player_rect.x = 0
+    if player_rect.x > 770:
+        player_x = 0
+        player_rect.x = 770
+
+    player_x = player_x*0.9
+    player_rect.x += player_x
     screen.blit(player_surface, player_rect)
 
     pygame.display.update()
