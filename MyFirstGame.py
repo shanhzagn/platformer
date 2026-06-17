2ad33cdadd1079686ade2775ef85cb92d923dc0d
shanhzagn <shawn.s.zhang.2011@gmail.com>
2026-06-17 18:22:49 +1000

    improved end screen

--- MyFirstGame.py
+++ MyFirstGame.py
@@ -6,6 +6,7 @@ def display_score():
     score_surface = test_font.render(f"{current_time}",False,(64,64,64))
     score_rect = score_surface.get_rect(center = (400,50))
     screen.blit(score_surface,score_rect)
+    return current_time
 
 pygame.init()
 screen = pygame.display.set_mode((800,400))
@@ -14,6 +15,7 @@ clock = pygame.time.Clock()
 test_font = pygame.font.Font(None, 50)
 game_active = False
 start_time = 0
+score = 0
 
 sky_surface = pygame.Surface((800,400))
 sky_surface.fill("darkslategray1")
@@ -21,8 +23,6 @@ ground_surface = pygame.Surface((800,60))
 ground_surface.fill("saddlebrown")
 grass_surface = pygame.Surface((800,10))
 grass_surface.fill("green3")
-#text_surface = test_font.render("Parkour Jump", False, (64,64,64))
-#text_rect = text_surface.get_rect(center = (400,50))
 
 obstacle_surface = pygame.Surface((50,50))
 obstacle_surface.fill("azure4")
@@ -34,9 +34,15 @@ player_surface.fill("blue")
 player_rect = player_surface.get_rect(bottomleft = (150,340))
 player_gravity = 0
 
-player_stand = pygame.Surface((50,50))
+player_stand = pygame.Surface((70,70))
 player_stand.fill("blue")
 player_stand_rect = player_stand.get_rect(center = (400,200))
+
+game_name = test_font.render("Square Jump",False,(111,196,169))
+game_name_rect = game_name.get_rect(center = (400,80))
+
+game_message = test_font.render("Press space to jump",False,(111,196,169))
+game_message_rect = game_message.get_rect(center = (400,320))
 
 while True:
     for event in pygame.event.get():
@@ -65,9 +71,7 @@ while True:
         screen.blit(sky_surface,(0,0))
         screen.blit(ground_surface,(0,340))
         screen.blit(grass_surface,(0,340))
-        #pygame.draw.rect(screen, "#c0e8ec", text_rect)
-        #screen.blit(text_surface,text_rect)
-        display_score()
+        score = display_score()
 
         player_gravity += 0.5
         player_rect.y += player_gravity
@@ -85,6 +89,14 @@ while True:
     else:
         screen.fill((94,129,162))
         screen.blit(player_stand,player_stand_rect)
+        screen.blit(game_name, game_name_rect)
+        score_message = test_font.render(f"Your score: {score}", False, (111, 196, 169))
+        score_message_rect = score_message.get_rect(center=(400, 320))
+
+        if score == 0:
+            screen.blit(game_message,game_message_rect)
+        elif score > 0:
+            screen.blit(score_message,score_message_rect)
 
     pygame.display.update()
     clock.tick(60)
